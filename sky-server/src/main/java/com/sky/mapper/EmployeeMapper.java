f56bf82be9875467682f1a56bd3505842a3af155
songzhiyong <songzhiyong97@gmail.com>
2023-09-06 21:25:44 +0800

    员工分页完成

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
-import com.sky.dto.EmployeeDTO;
+import com.github.pagehelper.Page;
+
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -20,5 +22,8 @@ public interface EmployeeMapper {
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user)" +
             " VALUES(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser}) ")
      void add(Employee employee);
+
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
 }
